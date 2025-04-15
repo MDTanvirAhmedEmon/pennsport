8b41e489a883dec5abdaa41433397420d53f1ad6
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-04-15 14:57:21 +0600

    done footer and header without mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ const Home = () => {
 
   return (
     <div className="background-gradient h-[90vh]">
-      <div className=' container mx-auto h-full  flex text-white font-bold items-center '>
-        <div className=' w-1/2'>
-          <h1 className=' text-8xl text-center  bg-gradient-to-r from-white via-[#ececec] to-[#636363] bg-clip-text text-transparent'>Science-Based</h1>
-          <h1 className=' text-8xl text-center bg-gradient-to-r from-white via-[#ececec] to-[#161616] bg-clip-text text-transparent pb-10'>Detailing</h1>
+      <div className=' container mx-auto h-full  flex flex-col md:flex-row justify-center text-white font-bold items-center '>
+        <div className=' md:w-1/2'>
+          <h1 className='text-5xl lg:text-7xl 2xl:text-8xl text-center  bg-gradient-to-r from-white via-[#ececec] to-[#636363] bg-clip-text text-transparent'>Science-Based</h1>
+          <h1 className='text-5xl lg:text-7xl 2xl:text-8xl text-center bg-gradient-to-r from-white via-[#ececec] to-[#161616] bg-clip-text text-transparent pb-10'>Detailing</h1>
         </div>
-        <div className=' w-1/2 flex justify-center items-center'>
-          <Image src={logo} className='w-[600px]' width={500} height={500} alt='logo' />
+        <div className=' md:w-1/2 flex justify-center items-center'>
+          <Image src={logo} className=' w-[350px] lg:w-[500px] 2xl:w-[600px] mt-6 md:mt-0' width={500} height={500} alt='logo' />
         </div>
       </div>
     </div>
--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,23 +1,29 @@
 import Image from 'next/image';
 import logo from '../../assets/logo.svg'
 import Link from 'next/link';
+import { RxHamburgerMenu } from 'react-icons/rx';
 const Header = () => {
     return (
-        <header className=' h-20 flex items-center -mb-20 z-50'>
-            <div className=' container mx-auto flex justify-between items-center'>
+        <header className=' h-16 md:h-20 flex items-center -mb-16 md:-mb-20 z-50'>
+            <div className=' px-3 w-full md:container md:mx-auto flex justify-between items-center'>
                 <Image
                     src={logo}
                     alt="Logo"
                     width={300}
                     height={300}
-                    className=" w-40"
+                    className=" w-32 md:w-40"
                 />
-                <div className=' flex items-center gap-10 text-xl'>
-                    <Link className=' text-white' href={`/`}>Home</Link>
-                    <Link className=' text-white' href={`/service`}>Service</Link>
-                    <Link className=' text-white' href={`/service`}>About Us</Link>
-                    <Link className=' text-white' href={`/service`}>Contact Us</Link>
-                    <Link className=' text-white' href={`/service`}>Pages</Link>
+                <div className='md:hidden'>
+                    <RxHamburgerMenu className=' w-7 h-7 text-white' />
+                </div>
+                <div className='hidden md:block'>
+                    <div className=' flex items-center gap-10 text-xl '>
+                        <Link className=' text-white' href={`/`}>Home</Link>
+                        <Link className=' text-white' href={`/service`}>Service</Link>
+                        <Link className=' text-white' href={`/service`}>About Us</Link>
+                        <Link className=' text-white' href={`/service`}>Contact Us</Link>
+                        <Link className=' text-white' href={`/service`}>Pages</Link>
+                    </div>
                 </div>
             </div>
         </header>
